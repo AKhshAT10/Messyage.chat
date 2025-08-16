79d82153911941f159ef35b6ead58ad4827384fe
Akhshat Sharma <akhshat.sharma25@gmail.com>
2025-08-16 14:28:33 +0530

    Added Routes for SignUp Login Auth and the logic

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import authRoutes from "./src/routes/auth.route.js";
 import { connectDB } from "./src/lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./src/routes/message.route.js";
+import cors from 'cors';
 
 //cookie parser helps us parse the cookie , unjumble it ig
 
@@ -16,6 +17,11 @@ const PORT = process.env.PORT||5001;
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true,
+}));
 
 app.use("/api/auth",authRoutes);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { Route , Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import { useAuthStore } from './store/useAuthStore';
+import { useEffect } from 'react';
+import {Loader} from 'lucide-react';
+import { Navigate } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className='text-red-500'>Hello World</div>
+  
+  const {authUser , checkAuth , isCheckingAuth} = useAuthStore();
+
+  useEffect(()=>{
+    checkAuth();
+  },[checkAuth]);
+
+  console.log({authUser});
+
+  if(isCheckingAuth && !authUser) return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className='size-10 animate-spin'/>
+      </div>
   )
+  return (
+    <div>
+
+        <NavBar/>
+
+        <Routes>
+          <Route path='/' element={authUser ? <HomePage/> : <Navigate to='/login'/>}/>
+          <Route path='/signup' element={!authUser ? <SignUpPage/> : <Navigate to='/'/>}/>
+          <Route path='/login' element={!authUser ?<LoginPage/> : <Navigate to='/'/>}/>
+          <Route path='/settings' element={<SettingsPage/>}/>
+          <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to='/login'/>}/>
+        </Routes>
+ 
+    </div>
+  );
 };
 
 export default App
